88784b703f32858ac36a6127f8612355fb969a46
Isaac Kennedy <isaac_kennedy@hotmail.com>
2018-10-02 09:44:14 -0300

    Adicionar arquivos de projeto.

---
+++ BaseConverter/BaseConverter.cpp
@@ -0,0 +1,12 @@
+﻿// BaseConverter.cpp: define o ponto de entrada para o aplicativo.
+//
+
+#include "BaseConverter.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Olá, CMake." << endl;
+	return 0;
+}
