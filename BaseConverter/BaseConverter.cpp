32ec083b4ebc157a323b89fe5f7f081e3125b79b
Isaac Kennedy isaken <isaac_kennedy@hotmail.com>
2018-10-02 13:51:56 -0300

    Finalização dos algoritmos de inversão de string, conversão de base e inicialização do alfabeto.

--- BaseConverter/BaseConverter.cpp
+++ BaseConverter/BaseConverter.cpp
@@ -1,12 +1,74 @@
 ﻿// BaseConverter.cpp: define o ponto de entrada para o aplicativo.
-//
 
 #include "BaseConverter.h"
 
-using namespace std;
-
 int main()
 {
-	cout << "Olá, CMake." << endl;
+	int input = 0;
+	int base = 0;
+	//cout << "Insira um valor: " << endl;
+	initialize_alphabet();
+
+	int control = 0;
+	while (control != 1)
+	{
+		clear_screen();
+		cout << "Insira o valor a ser convertido: ";
+		cin >> input;
+
+		cout << "Insira a base a ser convertida: ";
+		cin >> base;
+		
+		string final_value = invert_string(convert(input, base));
+
+		cout << input << " na base " << base << " = " << 
+			final_value << endl << endl;
+
+		cin.clear();
+		cin.ignore();
+
+		cout << "(1) para sair do programa.." << endl;
+		cout << "Qualquer outro valor para continuar..." << endl;
+		cin >> control;
+	}
+	
+
+	//Mantem a janela do console aberta ate que seja pressionado o enter
+	cin.get();
+	
 	return 0;
+}
+
+string convert(int input, int base) {
+	int n = input;
+	string result;
+	do {
+		result.push_back((((n % base) < 10) ? ((n % base) + '0') : (alphabet[(n % base)])));
+		n = (int)(n / base);
+	} while (n != 0);
+	return result;
+}
+
+/* @func Funcao que inicializa um map com todas as letras do alfabeto como valores a serem mapeados, sendo que suas chaves são os valores 
+os quais elas correspondem
+*/
+void initialize_alphabet() {
+	char val = 'A';
+	for (int i = 10; i < 36; i++) {
+		alphabet[i] = val++;
+	}
+}
+
+string invert_string(string s) {
+	int start = 0, end = s.size() - 1;
+
+	while (start < end) {
+		char tmp = s[start];
+		s[start] = s[end];
+		s[end] = tmp;
+		start++;
+		end--;
+	}
+
+	return s;
 }
--- BaseConverter/BaseConverter.h
+++ BaseConverter/BaseConverter.h
@@ -4,5 +4,21 @@
 #pragma once
 
 #include <iostream>
+#include <string>
+#include <map>
+#include <cstdlib>
 
 // TODO: Referencie os cabeçalhos adicionais de que seu programa precisa aqui.
+using std::string;
+using std::map;
+using std::cin;
+using std::cout;
+using std::endl;
+
+map<char, int> alphabet;
+
+string convert(int input, int base);
+void initialize_alphabet();
+string invert_string(string s);
+void clear_screen() { if (system("CLS")) system("clear"); }
+
